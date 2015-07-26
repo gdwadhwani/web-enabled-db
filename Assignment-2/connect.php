9e7d7526d01454fc17edd2df8767b023583dac57
Gaurav <gdwadhwani@gmail.com>
2015-07-26 14:22:25 -0400

    added the php mysql connection file

---
+++ Assignment-2/connect.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gaurav
+ * Date: 7/25/2015
+ * Time: 11:25 PM
+ */
+
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', 'gaurav7890');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'movies_db_733_747');
+
+$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD,
+    DB_NAME) OR die ('Could not connect to MySQL: ' .
+    mysqli_connect_error() );
